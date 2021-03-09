770e09c46b4d99485ebd60c4054a1afe06968a86
= <=>
2021-03-09 23:52:42 +0500

    Second commit

---
+++ build/precache-manifest.1d140f060da563e17e05c58d306c634b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a04060b4318fce52c62d",
+    "url": "/ReduxThunkApp-Breeds_Ecommerce/static/js/main.3689e304.chunk.js"
+  },
+  {
+    "revision": "1328959ce8cff11282bc",
+    "url": "/ReduxThunkApp-Breeds_Ecommerce/static/js/runtime~main.008b5da8.js"
+  },
+  {
+    "revision": "9a724255d8a158b31866",
+    "url": "/ReduxThunkApp-Breeds_Ecommerce/static/js/2.f1f3d7ca.chunk.js"
+  },
+  {
+    "revision": "8a918c30eb9e96d6bfe973b11ac7c8a6",
+    "url": "/ReduxThunkApp-Breeds_Ecommerce/index.html"
+  }
+];
